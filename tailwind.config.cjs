29a0abadc912e7e8ff4b25e2fb6ede728c630d2e
YasakaKanoko <87556577+YasakaKanoko@users.noreply.github.com>
2025-12-27 17:28:35 +0800

    feat: from vitepress to astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
+	darkMode: "class", // allows toggling dark mode manually
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: [
+					"LXGWWenKai",
+					"Roboto",
+					"sans-serif",
+					...defaultTheme.fontFamily.sans,
+				],
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
+};
